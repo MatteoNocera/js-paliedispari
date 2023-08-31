d6e21b56c9ef8230772265cf023135d5a6324104
MatteoNocera <matteo_nocera@tiscali.it>
2023-08-31 18:14:07 +0200

    ex_2
    find single letter of the word

---
+++ ex_2_pali/script.js
@@ -0,0 +1,32 @@
+/*
+👉  Palidroma
+Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+
+* Tools
+- prompt
+- let/const
+- console.log
+- function
+- if/else
+
+*/
+
+// Chiedere all’utente di inserire una parola
+
+const userWord = prompt('type your word');
+
+// Creare una funzione per capire se la parola inserita è palindroma
+
+let letter = '';
+let letterReverse = '';
+
+for (let i = 0; i < userWord.length; i++) {
+    const letter = userWord[i];
+    console.log(letter);
+}
+
+for (let i = userWord.length - 1; i >= 0; i--) {
+    const letterReverse = userWord[i];
+    console.log(letterReverse);
+}
