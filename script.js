a9ba62c11da35040fcb554b339971a01b01d160d
MatteoNocera <matteo_nocera@tiscali.it>
2023-08-31 15:26:01 +0200

    insert logic
    start to write the simple rules

--- script.js
+++ script.js
@@ -16,3 +16,37 @@ Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzion
 */
 
 
+// chiedi all'utente pari o dispari
+
+const userChoise = prompt('Scegli : pari o dispari');
+
+console.log(userChoise);
+
+// chiedi all'utente un numero da 1 a 5
+
+const userNumber = Number(prompt('inserisci un numero da 1 a 5'));
+
+console.log(userNumber);
+
+// crea un numero random da 1 a 5 per il pc
+
+const pcNumber = Math.floor(Math.random() * 5 + 1);
+
+console.log(pcNumber);
+
+// somma i due numeri
+
+const sommaNumeri = userNumber + pcNumber;
+
+console.log(sommaNumeri);
+
+// stabilisci se la somma è pari o dispari
+
+if (sommaNumeri % 2 === 0) {
+    console.log('pari');
+} else {
+    console.log('dispari');
+}
+
+// stampa il vincitore
+
