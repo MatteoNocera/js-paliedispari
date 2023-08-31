b11a3e45389472f5ccc73d70ecfa898594e21dfa
MatteoNocera <matteo_nocera@tiscali.it>
2023-08-31 18:52:12 +0200

    working on second

--- ex_2_pali/script.js
+++ ex_2_pali/script.js
@@ -14,19 +14,27 @@ Chiedere all’utente di inserire una parola Creare una funzione per capire se l
 
 // Chiedere all’utente di inserire una parola
 
-const userWord = prompt('type your word');
+const userWord = 'ciao' //prompt('type your word');
 
 // Creare una funzione per capire se la parola inserita è palindroma
 
-let letter = '';
-let letterReverse = '';
 
 for (let i = 0; i < userWord.length; i++) {
     const letter = userWord[i];
     console.log(letter);
+
 }
+
+
+
 
 for (let i = userWord.length - 1; i >= 0; i--) {
     const letterReverse = userWord[i];
     console.log(letterReverse);
+
+    console.log(userWord.length);
+
 }
+
+
+
